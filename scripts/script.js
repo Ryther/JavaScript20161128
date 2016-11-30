666b05fba7267ce80495d59b626a1b6140b2f937
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-11-30 22:13:27 +0100

    Esercizio completato

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ function nomeCognome(form) {
 	newDiv.innerHTML += "<BR><I>" + nome + " " + cognome + "</I>";
 	document.body.appendChild(newDiv);
 }
-			
+
 function calcoli(form) {
 	var num1 = parseInt(form.campo1.value);
 	var num2 = parseInt(form.campo2.value);
@@ -20,22 +20,22 @@ function calcoli(form) {
 
 function esercizio2(form) {
 	var sesso = form.sesso.value;
-	var compleanno = form.compleanno.value;
+	var compleanno = new Date(form.compleanno.value);
 	if (compleanno == "") {
 		alert("Inserire una data di nascita!");
 		return;
 	}
-	
+
 	if (sesso == "M") {
 		document.body.style.background = "lightblue";
 	} else {
 		document.body.style.background = "lightpink";
 	}
-	
+
 	var newDiv = document.createElement('div');
-	newDiv.innerHTML = "<BR>Hai " + calcolaEta(compleanno, false) + " anni e " + calcolaEta(compleanno, true) + " mesi!";
+	newDiv.innerHTML = "<BR>Hai " + calcolaEta(compleanno, false) + " anni" + (calcolaEta(compleanno, true)==0?"":" e " + calcolaEta(compleanno, true) + " mesi!");
 	document.body.appendChild(newDiv);
-	if (compleanno = Date()) {
+	if ((compleanno.getDate() + "/" + compleanno.getMonth()) == (new Date().getDate() + "/" + new Date().getMonth())) {
 		alert("A U G U R I ! ! !");
 	}
 }
@@ -43,26 +43,30 @@ function esercizio2(form) {
 function calcolaEta(data, isMonth) {
 	var oggi = new Date();
 	var compleanno = new Date(data);
-	
+
 	if (!isMonth) {
-		if ((oggi.getMonth() - compleanno.getMonth()) >= 0) {
-			if ((oggi.getDay() - compleanno.getDay()) >= 0) {
+		if ((oggi.getMonth() - compleanno.getMonth()) == 0) {
+			if ((oggi.getDate() - compleanno.getDate()) >= 0) {
 				return (oggi.getFullYear() - compleanno.getFullYear());
 			} else {
 				return (oggi.getFullYear() - compleanno.getFullYear())-1;
 			}
+		} else if ((oggi.getMonth() - compleanno.getMonth()) > 0){
+			return (oggi.getFullYear() - compleanno.getFullYear());
 		} else {
 			return (oggi.getFullYear() - compleanno.getFullYear())-1;
 		}
 	} else {
-		if ((oggi.getMonth() - compleanno.getMonth()) >= 0) {
-			if ((oggi.getDay() - compleanno.getDay()) >= 0) {
-				return (oggi.getFullYear() - compleanno.getFullYear());
+		if ((oggi.getMonth() - compleanno.getMonth()) == 0) {
+			if ((oggi.getDate() - compleanno.getDate()) >= 0) {
+				return (oggi.getMonth() - compleanno.getMonth());
 			} else {
-				return (oggi.getFullYear() - compleanno.getFullYear())-1;
+				return 1+compleanno.getMonth()-(compleanno.getMonth() - oggi.getMonth());
 			}
+		} else if ((oggi.getMonth() - compleanno.getMonth()) > 0){
+			return (oggi.getMonth() - compleanno.getMonth());
 		} else {
-			return (oggi.getFullYear() - compleanno.getFullYear())-1;
+			return 1+compleanno.getMonth()-(compleanno.getMonth() - oggi.getMonth());
 		}
 	}
 }
